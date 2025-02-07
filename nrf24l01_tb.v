f9bd8370f7796959a73f486407acbd8b9223b9b6
zaileshar <zaileshar@gmail.com>
2025-02-07 16:07:26 +0530

    update database schema

---
+++ nrf24l01_tb.v
@@ -0,0 +1,10 @@
+module nrf24l01_tb (
+    input clk,
+    input rst_n,
+    output reg out
+);
+    always @(posedge clk) begin
+        if (!rst_n) out <= 0;
+        else out <= ~out;
+    end
+endmodule
